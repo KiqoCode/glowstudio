c97ea74278a3c60cb3f84b6b962edbdc3ed92ce7
KiqoCode <cisco@twistedinc.se>
2022-07-25 22:38:12 +0200

    Align booking buttons differently on mobile and computer

--- src/components/Start.js
+++ src/components/Start.js
@@ -8,15 +8,15 @@ const Start = () => {
         <img src={require('assets/createbymattis-logo-400.png')} className='max-w-[25rem] mb:max-w-[20rem]' />
         <p className='font-["Playfair_Display"] text-mattis-pink text-4xl mt-10 text-center'>Salongen med det lilla extra</p>
         <p className='tracking-[10px] text-mattis-pink text-center font-thin mt-5 text-sm'>CREATE BY MATTIS</p>
-        <div className='mt-10'>
+        <div className='flex flex-row mb:flex-col mt-10 mb:justify-center items-center'>
           <button
             type="button"
-            className="bg-white uppercase px-10 py-5 mr-2 rounded-full text-mattis-pink outline-mattis-pink outline-1 outline hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            className="bg-white uppercase px-10 py-5 mr-2 mb-2 rounded-full text-mattis-pink outline-mattis-pink outline-1 outline hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               boka tid - stockholm
           </button>
           <button
             type="button"
-            className="bg-white uppercase px-10 py-5 ml-2 rounded-full text-mattis-pink outline-mattis-pink outline-1 outline hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            className="bg-white uppercase px-10 py-5 rounded-full text-mattis-pink outline-mattis-pink outline-1 outline hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               boka tid - dalarna
           </button>
         </div>
