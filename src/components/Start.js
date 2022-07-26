ddcd155dea685c8da339e7651a8e74b20e1ab734
KiqoCode <cisco@twistedinc.se>
2022-07-26 22:55:40 +0200

    Transitions on start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,30 +1,53 @@
-import React, {require} from 'react';
+import React, {require, useEffect, useState} from 'react';
+import { Transition } from '@headlessui/react';
 
 const Start = () => {
+
+  const [showStart, setShowStart] = useState(false);
+
+  useEffect(() => {
+    setShowStart(true);
+  },[]);
 
   return (
     <div className='flex h-full w-full items-center justify-start mb:justify-center pl-[17vw] mb:pl-0 pb-32'>
       <div className='flex flex-col items-center justify-center z-0'>
-        <img src={require('assets/createbymattis-logo-400.png')} className='max-w-[25rem] mb:max-w-[20rem]' />
-        <p className='font-["Playfair_Display"] text-mattis-pink text-4xl mt-10 text-center'>Salongen med det lilla extra</p>
-        <p className='tracking-[10px] text-mattis-pink text-center font-thin mt-5 text-sm'>CREATE BY MATTIS</p>
-        <div className='flex flex-row mb:flex-col mt-10 mb:justify-center items-center'>
+        <Transition
+          show={showStart}
+          enter="transition ease-in-out duration-[1500ms] transform"
+          enterFrom="opacity-0 -translate-y-full"
+          enterTo="opacity-100 translate-y-0"
+          className="flex flex-col items-center justify-center z-0"
+        >
+          <img src={require('assets/createbymattis-logo-400.png')} className='max-w-[25rem] mb:max-w-[20rem]' />
+          <p className='font-["Playfair_Display"] text-mattis-pink text-4xl mt-10 text-center'>Salongen med det lilla extra</p>
+          <p className='tracking-[10px] text-mattis-pink text-center font-thin mt-5 text-sm'>CREATE BY MATTIS</p>
+        </Transition>
+        <Transition
+          show={showStart}
+          enter="transition ease-in-out duration-[1500ms] transform"
+          enterFrom="opacity-0 translate-y-full"
+          enterTo="opacity-100 translate-y-0"
+          className="flex flex-col items-center justify-center z-0"
+        >
+          <div className='flex flex-row mb:flex-col mt-10 mb:justify-center items-center'>
+            <button
+              type="button"
+              className="bg-white text-mattis-pink px-10 py-5 rounded-full border border-mattis-pink uppercase mr-1 mb:mr-0 hover:bg-mattis-pink hover:text-white transition-colors duration-200">
+                boka tid - stockholm
+            </button>
+            <button
+              type="button"
+              className="bg-white text-mattis-pink px-10 py-5 rounded-full border border-mattis-pink uppercase mt-2 hover:bg-mattis-pink hover:text-white transition-colors duration-200">
+                boka tid - dalarna
+            </button>
+          </div>
           <button
             type="button"
-            className="bg-white text-mattis-pink px-10 py-5 rounded-full border border-mattis-pink uppercase mr-1 mb:mr-0 hover:bg-mattis-pink hover:text-white transition-colors duration-200">
-              boka tid - stockholm
+            className="bg-mattis-pink px-20 py-5 rounded-full uppercase text-white mt-5 hover:bg-white hover:text-mattis-pink border hover:border-mattis-pink transition-colors duration-200">
+              webshop
           </button>
-          <button
-            type="button"
-            className="bg-white text-mattis-pink px-10 py-5 rounded-full border border-mattis-pink uppercase mt-2 hover:bg-mattis-pink hover:text-white transition-colors duration-200">
-              boka tid - dalarna
-          </button>
-        </div>
-        <button
-          type="button"
-          className="bg-mattis-pink px-20 py-5 rounded-full uppercase text-white mt-5 hover:bg-white hover:text-mattis-pink border hover:border-mattis-pink transition-colors duration-200">
-            webshop
-        </button>
+        </Transition>
       </div>
     </div>
   );
