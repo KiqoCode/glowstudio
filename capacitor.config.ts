5852bcf3117f475d51c5c12b874ad1878e71096e
Cisco Osterman <cisco@twistedinc.se>
2022-11-11 15:36:04 +0100

    Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'se.createbymattis.app',
+  appName: 'createbymattis',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
